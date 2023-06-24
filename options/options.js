e78c5ab22a67c1936ae1317d29a9beefc4a3070c
Crowdbotics <team@crowdbotics.com>
2023-06-24 23:57:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-paper-41720",
+  url: "https://wild-paper-41720.botics.co",
+  api: "https://wild-paper-41720.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
